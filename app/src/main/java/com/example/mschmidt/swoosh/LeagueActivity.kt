93b3d52db6abcdc92522e653fe5f0e992d14913d
Mateus Schmidt <mateusschmidt3@hotmail.com>
2018-04-10 15:57:33 -0300

    second intent

--- app/src/main/java/com/example/mschmidt/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/mschmidt/swoosh/LeagueActivity.kt
@@ -3,12 +3,20 @@ package com.example.mschmidt.swoosh
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
+    }
+
+
 }
